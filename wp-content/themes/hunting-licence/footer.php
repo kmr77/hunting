b2c7d076cb35042a0022bcb0998052e8bead1c6e
KIMURA MASAYUKI <>
2025-03-31 10:34:48 +0900

    初回コミット

---
+++ wp-content/themes/hunting-licence/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="inner">
+        <div class="copy">©️hunter-license.com</div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ wp-content/themes/hunting-licence/header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<!-- <html <?php language_attributes(); ?>> -->
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>狩猟免許試験例題集（狩猟免許試験過去問集）</title>
+  <meta name="description" content="狩猟免許試験の過去問を猟具別に掲載している過去問サイトです。狩猟免許とはどういう免許なのかを学ぶこともできます。">
+  <meta name="keywords" content="狩猟免許,狩猟免許試験,過去問,例題集,テキスト,猟具,法令,一種銃猟,二種銃猟,網猟,あみ猟,罠猟,わな猟,空気銃">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link rel="stylesheet" href="<?php bloginfo ('stylesheet_url'); ?>" type="text/css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css" type="text/css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/common.css" type="text/css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/top.css" type="text/css" />
+  <!-- JS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js?ver=1.8.3"></script>
+</head>
+
+<body>
+  <a class="pagetop" href="#">
+    <div class="arrow"></div>
+  </a>
+  <div class="container">
+    <header>
+      <div class="inner">
+        <div class="header-nav">
+          <ul>
+            <li>
+              <div class="logo">ここにロゴ</div>
+            </li>
+            <li>
+              <button class="button" data-modal-open="modal-1">MENU</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+      
+    <div class="modal" id="modal-1">
+      <div class="modal-overlay" data-modal-close>
+        <div class="modal-container">
+          <h2 class="modal-title">MENU</h2>
+          <div class="modal-content">
+            <div class="modal-nav">
+              <ul>
+                <li><a href="index.html">狩猟免許試験過去問題集</a></li>
+                <li><a href="know.html">知っておくべきこと</a></li>
+                <li><a href="application.html">狩猟免許受験申請</a></li>
+                <li><a href="content.html">狩猟免許試験の内容と対策</a></li>
+                <li><a href="registration.html">狩猟者登録</a></li>
+                <li><a href="hourei.html">法令編〜全問題〜</a></li>
+                <li><a href="only-1.html">第一種猟銃のみ問題</a></li>
+                <li><a href="only-2.html">第二種猟銃のみ問題</a></li>
+                <li><a href="ami.html">網猟問題のみ</a></li>
+                <li><a href="wana.html">わな猟問題のみ</a></li>
+                <li><a href="kousa01.html">猟銃等講習会 考査1</a></li>
+                <li><a href="kousa02.html">猟銃等講習会 考査2</a></li>
+                <li><a href="kousa03.html">猟銃等講習会 考査3</a></li>
+                <li><a href="kousa04.html">猟銃等講習会 考査4</a></li>
+                <li><a href="number.html">数字問題</a></li>
+              </ul>
+            </div>
+          </div>
+            <div class="modal-footer">
+              <button class="button" data-modal-close="modal-1">
+                閉じる
+              </button>
+            </div>
+          </div>
+        </div>
+
+      <div class="inner">
+        <nav class="breadcrumb">
+          <ul>
+            <li>
+              <a href="index.html">狩猟免許過去問</a>
+            </li>
+            <li>
+              <a>網猟 過去問</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
