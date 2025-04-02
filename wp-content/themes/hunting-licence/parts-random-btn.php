0e6735fbcd01bd23d27a70f07a5ee3f81f90812a
KIMURA MASAYUKI <>
2025-04-02 16:55:56 +0900

    ✨ ランダムボタンをパーツ化

---
+++ wp-content/themes/hunting-licence/parts-random-btn.php
@@ -0,0 +1,5 @@
+<div class="random-btn">
+               <button id="toggle-random" class="edit-btn">
+                   <?php echo (isset($_GET['random']) && $_GET['random'] == '1') ? '通常順に戻す' : 'ランダムに表示'; ?>
+               </button>
+             </div>
