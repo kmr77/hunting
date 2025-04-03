d635f54969ccb1053b156fa2d2d0954ce9ad79fb
KIMURA MASAYUKI <>
2025-04-03 16:09:03 +0900

    ♻️ 修正

--- wp-content/themes/hunting-licence/parts-breadcrumb.php
+++ wp-content/themes/hunting-licence/parts-breadcrumb.php
@@ -1,7 +1,7 @@
 <nav class="breadcrumb">
           <ul>
             <li>
-              <a href="/">狩猟免許過去問</a>
+              <a href="/huntinglicence">狩猟免許過去問</a>
             </li>
             <li>
               <a><?php the_title(); ?></a>
