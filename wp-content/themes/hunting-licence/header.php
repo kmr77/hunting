a038e55300edd63a7ea567b2e4d02917042f3af9
KIMURA MASAYUKI <>
2025-04-03 12:35:06 +0900

    ✨ リンク先変更

--- wp-content/themes/hunting-licence/header.php
+++ wp-content/themes/hunting-licence/header.php
@@ -48,21 +48,19 @@
           <div class="modal-content">
             <div class="modal-nav">
               <ul>
-                <li><a href="index.html">狩猟免許試験過去問題集</a></li>
-                <li><a href="know.html">知っておくべきこと</a></li>
-                <li><a href="application.html">狩猟免許受験申請</a></li>
-                <li><a href="content.html">狩猟免許試験の内容と対策</a></li>
-                <li><a href="registration.html">狩猟者登録</a></li>
-                <li><a href="hourei.html">法令編〜全問題〜</a></li>
-                <li><a href="only-1.html">第一種猟銃のみ問題</a></li>
-                <li><a href="only-2.html">第二種猟銃のみ問題</a></li>
-                <li><a href="ami.html">網猟問題のみ</a></li>
-                <li><a href="wana.html">わな猟問題のみ</a></li>
-                <li><a href="kousa01.html">猟銃等講習会 考査1</a></li>
-                <li><a href="kousa02.html">猟銃等講習会 考査2</a></li>
-                <li><a href="kousa03.html">猟銃等講習会 考査3</a></li>
-                <li><a href="kousa04.html">猟銃等講習会 考査4</a></li>
-                <li><a href="number.html">数字問題</a></li>
+                <li><a href="/">狩猟免許試験過去問題集</a></li>
+                <li><a href="../know/">知っておくべきこと</a></li>
+                <li><a href="../application/">狩猟免許受験申請</a></li>
+                <li><a href="../content/">狩猟免許試験の内容と対策</a></li>
+                <li><a href="../registration/">狩猟者登録</a></li>
+                <li><a href="../all/">全問題</a></li>
+                <li><a href="../laws/">法令問題</a></li>
+                <li><a href="../type1/">一種猟銃問題</a></li>
+                <li><a href="../type2/">二種猟銃問題</a></li>
+                <li><a href="../net/">網（あみ）猟問題</a></li>
+                <li><a href="../trap/">罠（わな）猟問題</a></li>
+                <li><a href="../examination/">猟銃等講習会 考査問題</a></li>
+                <li><a href="../numbers/">数字問題</a></li>
               </ul>
             </div>
           </div>
