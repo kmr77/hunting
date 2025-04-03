fec7defcf2562c2bdc5825e26a3e8368fd8253c4
KIMURA MASAYUKI <>
2025-04-03 12:35:43 +0900

    ✨ 各ページ作成

--- wp-content/themes/hunting-licence/page-ami.php
+++ wp-content/themes/hunting-licence/page-ami.php
@@ -1,25 +1,17 @@
 <?php 
 /*
-Template Name: あみ
+Template Name: あみ猟問題
 */
 get_header(); ?>
 <div class="inner">
-        <nav class="breadcrumb">
-          <ul>
-            <li>
-              <a href="index.html">狩猟免許過去問</a>
-            </li>
-            <li>
-              <a>網猟 過去問</a>
-            </li>
-          </ul>
-        </nav>
+        <!-- パンくずパーツ -->
+        <?php get_template_part('parts-breadcrumb'); ?>
       </div>
     </div>
     <div class="main-visual ami">
       <div class="inner">
-        <h1>網猟のみ｜狩猟免許試験過去問集</h1>
-        <p>狩猟免許試験の過去問題を猟具ごとに整理し、全ての問題に答えと詳しい解説を網羅しています。また、スマートフォン対応により、移動中やテスト直前でも手軽に復習できる設計となっており、効率的な暗記や知識の定着に役立ちます。これにより、狩猟免許試験の準備をいつでもどこでも進めることができ、受験者が合格に向けた学習を効率的に行えるようサポートしています。</p>
+        <h1><?php the_title(); ?>｜狩猟免許試験過去問集</h1>
+        <p>網猟に関する過去問を厳選し、解答と詳細な解説を付けた問題集を提供。試験頻出の知識を効率的に学べるよう構成されています。特に、網猟特有の法規制や使用できる猟具に関する問題を重点的にカバー。スマートフォン対応で、移動時間やスキマ時間を活用しながら知識を定着させることができます。狩猟免許試験の網猟分野で確実に得点できるよう、合格を目指す受験生を徹底サポートします。</p>
       </div>
     </div>
     <div class="inner">
@@ -27,9 +19,8 @@ get_header(); ?>
         <p>狩猟免許試験は、法令関連の問題が
           <strong>全30問中13問</strong>出題されます。言葉が難しかったり紛らわしいものもありますが、法令関連の出来が合否をわけますのでしっかり準備しましょう。今回は
           <strong>網猟</strong>だけに出題される問題を練習します。全猟種共通の問題は
-          <a href="hourei.html" target="_blank">こちら</a>で見ることができます。</p>
-        <p>2024年の例題集から抜粋していますので、法律が改訂されたりして答えが違う場合はご指摘いただけると幸いです。</p>
-        <p class="annotation">※問題文をクリックすると選択肢・解答が表示されます。</p>
+          <a href="laws" target="_blank">こちら</a>で見ることができます。</p>
+        <?php get_template_part('parts-infotext'); ?>
         <!-- 問題ここから -->
         <div class="accordion-inner">
           <dl id="accordion">
@@ -46,8 +37,8 @@ get_header(); ?>
             
             // 投稿のループを開始
             $args = array(
-                'category_name' => 'net', // カテゴリスラッグ「net」の記事のみ取得
-                //'post_type' => 'post', // カテゴリスラッグ「net」の記事のみ取得
+                'category_name' => 'ami', // カテゴリスラッグ「ami」の記事のみ取得
+                //'post_type' => 'post', // カテゴリスラッグ「ami」の記事のみ取得
                 'posts_per_page' => -1,   // すべての投稿を表示（ページネーションなし）
                 'orderby' => $orderby,    // ランダム or ID順
                 'order' => $order
@@ -71,9 +62,9 @@ get_header(); ?>
                         <dl>
                             <dt class="select-dt">
                                 <ul>
-                                    <li>ア：<?php the_field('select_a'); ?></li>
-                                    <li>イ：<?php the_field('select_i'); ?></li>
-                                    <li>ウ：<?php the_field('select_u'); ?></li>
+                                    <li><span class="bold">ア：</span><?php the_field('select_a'); ?></li>
+                                    <li><span class="bold">イ：</span><?php the_field('select_i'); ?></li>
+                                    <li><span class="bold">ウ：</span><?php the_field('select_u'); ?></li>
                                 </ul>
                                 <button class="answer-btn">答えを開閉</button>
                             </dt>
--- wp-content/themes/hunting-licence/page-numbers.php
+++ wp-content/themes/hunting-licence/page-numbers.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-Template Name: 法令問題
+Template Name: 数字問題
 */
 get_header(); ?>
 <div class="inner">
@@ -8,18 +8,19 @@ get_header(); ?>
         <?php get_template_part('parts-breadcrumb'); ?>
       </div>
     </div>
-    <div class="main-visual laws">
+    <div class="main-visual ami">
       <div class="inner">
         <h1><?php the_title(); ?>｜狩猟免許試験過去問集</h1>
-        <p>狩猟免許試験の法令問題を徹底分析し、過去問を体系的に整理。すべての問題に正確な解答と詳細な解説を付し、試験対策に必要な知識を網羅しています。さらに、スマートフォン対応で移動中でも手軽に復習可能。狩猟法や関連法規の理解を深め、実際の試験で問われる重要ポイントを確実に押さえることができます。これにより、受験者は効率的に学習を進め、合格への道を確実なものにすることができます。</p>
+        <p>網猟に関する過去問を厳選し、解答と詳細な解説を付けた問題集を提供。試験頻出の知識を効率的に学べるよう構成されています。特に、網猟特有の法規制や使用できる猟具に関する問題を重点的にカバー。スマートフォン対応で、移動時間やスキマ時間を活用しながら知識を定着させることができます。狩猟免許試験の網猟分野で確実に得点できるよう、合格を目指す受験生を徹底サポートします。</p>
       </div>
     </div>
     <div class="inner">
       <h2>練習問題</h2>
         <p>狩猟免許試験は、法令関連の問題が
-          <strong>全30問中13問</strong>出題されます。言葉が難しかったり紛らわしいものもありますが、法令関連の出来が合否をわけますのでしっかり準備しましょう。このページでは
-          <strong>法令問題</strong>を中心に全問題を網羅しています。</p>
-          <?php get_template_part('parts-infotext'); ?>
+          <strong>全30問中13問</strong>出題されます。言葉が難しかったり紛らわしいものもありますが、法令関連の出来が合否をわけますのでしっかり準備しましょう。今回は
+          <strong>網猟</strong>だけに出題される問題を練習します。全猟種共通の問題は
+          <a href="laws" target="_blank">こちら</a>で見ることができます。</p>
+        <?php get_template_part('parts-infotext'); ?>
         <!-- 問題ここから -->
         <div class="accordion-inner">
           <dl id="accordion">
@@ -36,8 +37,7 @@ get_header(); ?>
             
             // 投稿のループを開始
             $args = array(
-                'category_name' => 'laws', // カテゴリスラッグ「net」の記事のみ取得
-                //'post_type' => 'post', // カテゴリスラッグ「net」の記事のみ取得
+                'category_name' => 'numbers', // カテゴリスラッグ「numbers」の記事のみ取得
                 'posts_per_page' => -1,   // すべての投稿を表示（ページネーションなし）
                 'orderby' => $orderby,    // ランダム or ID順
                 'order' => $order
@@ -58,20 +58,8 @@ get_header(); ?>
                         <!-- <button class="openclose-btn">選択肢を開閉</button> -->
                     </dt>
                     <dd>
-                        <dl>
-                            <dt class="select-dt">
-                                <ul>
-                                    <li><span class="bold">ア：</span><?php the_field('select_a'); ?></li>
-                                    <li><span class="bold">イ：</span><?php the_field('select_i'); ?></li>
-                                    <li><span class="bold">ウ：</span><?php the_field('select_u'); ?></li>
-                                </ul>
-                                <button class="answer-btn">答えを開閉</button>
-                            </dt>
-                            <dd class="answer-dd">
-                                <span class="answer">答）<?php the_field('answer'); ?><br>
-                                <?php the_field('answer_body'); ?></span>
-                            </dd>
-                        </dl>
+                        <span class="answer">答）<?php the_field('answer'); ?><br>
+                        <?php the_field('answer_body'); ?></span>
                     </dd>
                     <?php
                     // カウンターを1増やす
